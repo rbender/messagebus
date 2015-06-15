5c31f58202efd6169bed143784b9e1e7899bdd4c
Robert Bender <robert_bender@comcast.com>
2015-06-15 11:02:04 -0400

    Add optional command lines parameters.
    The app needed a way to specify the port and
    config path on the command line. Likewise, the
    simple sensor publisher needed a way to
    pass the serial port, baud rate and message bus
    URL on the command line as well.

--- messagebus/publishers/simple_sensor_publisher_process.py
+++ messagebus/publishers/simple_sensor_publisher_process.py
@@ -1,9 +1,14 @@
 
+import argparse
 import logging
 import serial
 
 from messagebus.client import MessageBusClient
 from messagebus.util.simple_sensor_parser import SimpleSensorParser
+
+DEFAULT_PORT = "/dev/tty.usbmodem1411"
+DEFAULT_BAUD = 9600
+DEFAULT_URL = "http://127.0.0.1:8000/post_message"
 
 class SimpleSensorPublisherProcess():
 
@@ -24,6 +29,14 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG)
 
-    connection = serial.Serial("/dev/tty.usbmodem1411", 9600)
-    publisher = SimpleSensorPublisherProcess(connection, "http://127.0.0.1:8000/post_message")
+    parser = argparse.ArgumentParser(description='Read sensor readings and pass them to the messagebus.')
+    parser.add_argument('-p', '--port', action="store", default=DEFAULT_PORT, help='Serial port')
+    parser.add_argument('-b', '--baud', action="store", default=DEFAULT_BAUD, type=int, help='Baud rate')
+    parser.add_argument('-u', '--url', action="store", default=DEFAULT_URL, help='Messagebus URL')
+
+    args = parser.parse_args()
+    logging.debug(args)
+
+    connection = serial.Serial(args.port, args.baud)
+    publisher = SimpleSensorPublisherProcess(connection, args.url)
     publisher.start()
--- web_main.py
+++ web_main.py
@@ -1,7 +1,5 @@
 import logging
-import sys
-import time
-
+import argparse
 from webapp import server
 
 from messagebus import MessageBus
@@ -9,7 +7,17 @@ from messagebus.services.store import MemoryMessageStore
 from messagebus.services import DeviceRegistry
 from messagebus.configuration.init_script_loader import load_init_scripts, shutdown_scripts
 
+DEFAULT_PORT = 8007
+CONFIG_ROOT = "config"
+
 logging.basicConfig(level=logging.DEBUG)
+
+parser = argparse.ArgumentParser(description='Read sensor readings and pass them to the messagebus.')
+parser.add_argument('-p', '--port', action="store", default=DEFAULT_PORT, type=int, help='HTTP port to run on')
+parser.add_argument('-c', '--config', action="store", default=CONFIG_ROOT, help='Path to configuration')
+
+args = parser.parse_args()
+logging.debug(args)
 
 # Initialize the message store
 message_store = MemoryMessageStore()
@@ -18,7 +26,7 @@ server.message_store = message_store
 # Load and populate DeviceMetadataService from the config/devices
 # directory. In the future, this path should be configurable
 devices = DeviceRegistry()
-devices.load_from_path("config/devices")
+devices.load_from_path(args.config + "/devices")
 server.devices = devices
 
 # Initialize the message bus
@@ -31,7 +39,7 @@ context["messagebus"] = message_bus
 context["device_registry"] = devices
 context["message_store"] = message_store
 
-load_init_scripts("config/init_scripts", context)
+load_init_scripts(args.config + "/init_scripts", context)
 
 # Start flash web application
-server.app.run(port=8000)
+server.app.run(port=args.port)
