48e73e2772bb776a8f9f2f463bd6a9999d6f4040
Robert Bender <robert_bender@comcast.com>
2015-06-30 12:31:29 -0400

    Add command-line interface to client.py
    client.py can now be invoked from the command line to post a
    new message to the bus.

--- messagebus/client/client.py
+++ messagebus/client/client.py
@@ -1,6 +1,13 @@
 import requests
 import logging
+import argparse
+import json
 
+from messagebus import Event
+from messagebus.util import date_time_utils
+
+
+DEFAULT_URL = "http://127.0.0.1:8007/post_message"
 
 class MessageBusClient:
 
@@ -19,3 +26,30 @@ class MessageBusClient:
         logging.debug("Response status code %s", response.status_code)
         if response.status_code > 200:
             logging.debug(response.text)
+
+        data = response.json()
+        return data["message_id"]
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description='Read sensor readings and pass them to the messagebus.')
+    parser.add_argument('-u', '--url', action="store", default=DEFAULT_URL, help='Messagebus URL')
+    parser.add_argument("-t", "--type", action="store", required=True, help="Message type")
+    parser.add_argument("-s", "--source", action="store", required=True, help="Message source")
+    parser.add_argument("-d", "--data", action="store", default="{}", help="Message Data (JSON)")
+
+    args = parser.parse_args()
+    logging.debug(args)
+
+    client = MessageBusClient(args.url)
+
+    payload = json.loads(args.data)
+
+    event = Event(source=args.source, type=args.type, data=payload, timestamp=date_time_utils.timestamp())
+
+    id = client.send_message(event)
+
+    logging.debug("Sent event {}".format(id))
+
